60a97fe16e07b034639b6727178b5d870b9e9e05
sanjithvishal <sanjithvishal.ec21@bitsathy.ac.in>
2025-05-12 00:47:37 +0530

    Added Junit+Mockito for OrderService

--- OrderService/src/main/java/com/example/demo/dto/UserDTO.java
+++ OrderService/src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,8 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private int userId;
     private String name;
