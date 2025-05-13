f85581fae094f70c5a20edfa82bc3864aeed2215
sanjithvishal <sanjithvishal.ec21@bitsathy.ac.in>
2025-05-13 18:31:05 +0530

    Update in UserService

--- UserService/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ UserService/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -2,22 +2,25 @@ package com.example.demo.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import com.netflix.discovery.converters.Auto;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 import com.example.demo.exceptions.UserNotFoundException;
 
 @Service
-@NoArgsConstructor
 public class UserServiceImpl implements UserService {
 
-	Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+	private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
+	@Autowired
 	private UserRepository repository;
 
 	// Saves a user to the repository.
